66a415d4dcac614e72eebcb7ba5ff24d7d3874db
Ion Stici <ionsticidev@gmail.com>
2023-02-21 09:39:18 +0200

    Update Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/base.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render("Hello");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// “ I’ve been interested in coding for a while but never taken the jump, until now.
+// I couldn’t recommend this course enough. I’m now in the job of my dreams and so
+// excited about the future. ”
+
+// Tanya Sinclair
+// UX Engineer
+
+// “ If you want to lay the best foundation possible I’d recommend taking this course.
+// The depth the instructors go into is incredible. I now feel so confident about
+// starting up as a professional developer. ”
+
+// John Tarkpor
+// Junior Front-end Developer
+
+// Dark Blue: hsl(240, 38%, 20%)
+// Grayish Blue: hsl(240, 18%, 77%)
+// Font size: 32px
+// Weights: 300, 500, 700
